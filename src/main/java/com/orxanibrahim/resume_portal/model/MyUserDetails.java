c1fa64d7bcc95d68a362baa3f3e9c8876145d067
ibrahimot <orxan.ibrahimov@kapitalbank.az>
2022-12-09 02:12:11 +0400

    added MyUserDetails and MyUserDetailsService classes

---
+++ src/main/java/com/orxanibrahim/resume_portal/model/MyUserDetails.java
@@ -0,0 +1,60 @@
+package com.orxanibrahim.resume_portal.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MyUserDetails implements UserDetails {
+    private String userName;
+    private String password;
+    private boolean isActive;
+
+    public MyUserDetails(User user) {
+        this.userName = user.getUserName();
+        this.password = user.getPassword();
+        this.isActive = user.getActive() ;
+        this.grantedAuthorities = Arrays.stream(user.getRoles().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+    private List<GrantedAuthority> grantedAuthorities;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return grantedAuthorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive;
+    }
+}
