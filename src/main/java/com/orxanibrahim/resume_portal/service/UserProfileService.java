bb610c98debabca4e26254d0319b2bd8fbbb59da
ibrahimot <orxan.ibrahimov@kapitalbank.az>
2022-12-18 01:49:31 +0400

    displayed user's work experience section temporarily added isCurrentJob boolean flag to fix the date in user's current workplace jobDescription field added to display the description of job in specific company

--- src/main/java/com/orxanibrahim/resume_portal/service/UserProfileService.java
+++ src/main/java/com/orxanibrahim/resume_portal/service/UserProfileService.java
@@ -1,6 +1,6 @@
 package com.orxanibrahim.resume_portal.service;
 
-import com.orxanibrahim.resume_portal.model.Jobs;
+import com.orxanibrahim.resume_portal.model.Job;
 import com.orxanibrahim.resume_portal.model.UserProfile;
 import com.orxanibrahim.resume_portal.repository.UserProfileRepository;
 import lombok.RequiredArgsConstructor;
@@ -8,9 +8,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -34,14 +31,14 @@ public class UserProfileService {
         userProfileOptional.orElseThrow(() -> new RuntimeException("user not found : "));
 
         var userProfile1 = userProfileOptional.get();
-        Jobs job1 = Jobs.builder()
+        Job job1 = Job.builder()
                 .company("company 1")
                 .designation("Designation 1")
                 .id(1)
                 .startDate(LocalDate.of(2018,10,20))
                 .endDate(LocalDate.of(2020,8,22))
                 .build();
-        Jobs job2 = Jobs.builder()
+        Job job2 = Job.builder()
                 .company("company 2")
                 .designation("Designation 2")
                 .id(2)
